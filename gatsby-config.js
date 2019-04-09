a58737ded79e3fbb170d704e4a568d02de145fdd
iammarkps <markkung08@gmail.com>
2019-04-09 18:58:44 +0700

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-74722377-4',
       },
     },
     `gatsby-plugin-feed`,
